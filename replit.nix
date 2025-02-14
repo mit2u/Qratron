14670864078076291051e991ed0168193c8bdffe
MitulRawat <5369743-MitulRawat@users.noreply.replit.com>
2025-02-14 12:08:36 +0000

    User checkpoint: Configure Replit environment: add port mappings and install additional dependencies.

---
+++ replit.nix
@@ -0,0 +1,22 @@
+{pkgs}: {
+  deps = [
+    pkgs.imagemagick_light
+    pkgs.zlib
+    pkgs.c-ares
+    pkgs.bash
+    pkgs.util-linux
+    pkgs.libuv
+    pkgs.pgadmin4
+    pkgs.libxcrypt
+    pkgs.grpc
+    pkgs.re2
+    pkgs.oneDNN
+    pkgs.glibcLocales
+    pkgs.cacert
+    pkgs.pkg-config
+    pkgs.openssl
+    pkgs.rustc
+    pkgs.libiconv
+    pkgs.cargo
+  ];
+}
